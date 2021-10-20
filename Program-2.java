ffc0a957e9370488aabb0cec1ca05c174c7f33ae
Lokesh516 <53927436+Lokesh516@users.noreply.github.com>
2021-10-20 14:30:54 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class  Odd
+{
+	public static void main(String[] args) 
+	{
+		int i=1,j;
+		Scanner sc=new Scanner(System.in);
+		int a =sc.nextInt();
+		for(j=1;j<=a;j++)
+		{
+
+			
+				System.out.println(i);
+				i=i+2;
+		}
+	}
+}
